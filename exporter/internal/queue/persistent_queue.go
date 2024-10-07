34cebaa6c3cb5f2df5443f69183d1ddc410e65f7
Sindy Li <sindy.li@snowflake.com>
2024-10-07 12:48:47 -0700

    POC

--- exporter/internal/queue/persistent_queue.go
+++ exporter/internal/queue/persistent_queue.go
@@ -189,10 +189,35 @@ func (pq *persistentQueue[T]) restoreQueueSizeFromStorage(ctx context.Context) (
 	return bytesToItemIndex(val)
 }
 
+func (pq *persistentQueue[T]) Read() (T, bool, func(error)) {
+	var (
+		req                  T
+		onProcessingFinished func(error)
+		consumed             bool
+	)
+
+	// If we are stopped we still process all the other events in the channel before, but we
+	// return fast in the `getNextItem`, so we will free the channel fast and get to the stop.
+	_, ok := pq.sizedChannel.pop(func(permanentQueueEl) int64 {
+		req, onProcessingFinished, consumed = pq.getNextItem(context.Background())
+		if !consumed {
+			return 0
+		}
+		return pq.set.Sizer.Sizeof(req)
+	})
+	if !ok {
+		var t T
+		return t, false, nil
+	}
+	return req, true, onProcessingFinished
+}
+
 // Consume applies the provided function on the head of queue.
 // The call blocks until there is an item available or the queue is stopped.
 // The function returns true when an item is consumed or false if the queue is stopped.
 func (pq *persistentQueue[T]) Consume(consumeFunc func(context.Context, T) error) bool {
+	// If we are stopped we still process all the other events in the channel before, but we
+	// return fast in the `getNextItem`, so we will free the channel fast and get to the stop.
 	for {
 		var (
 			index    uint64
@@ -348,6 +373,16 @@ func (pq *persistentQueue[T]) getNextItem(ctx context.Context) (uint64, T, bool)
 	// Increase the reference count, so the client is not closed while the request is being processed.
 	// The client cannot be closed because we hold the lock since last we checked `stopped`.
 	pq.refClient++
+	onProcessingFinished := func(consumeErr error) {
+		// Delete the item from the persistent storage after it was processed.
+		pq.mu.Lock()
+		// Always unref client even if the consumer is shutdown because we always ref it for every valid request.
+		defer func() {
+			if err = pq.unrefClient(ctx); err != nil {
+				pq.logger.Error("Error closing the storage client", zap.Error(err))
+			}
+			pq.mu.Unlock()
+		}()
 
 	return index, request, true
 }
@@ -384,7 +419,8 @@ func (pq *persistentQueue[T]) OnProcessingFinished(index uint64, consumeErr erro
 
 	// Ensure the used size and the channel size are in sync.
 	pq.sizedChannel.syncSize()
-
+	pq.refClient++
+	return request, onProcessingFinished, true
 }
 
 // retrieveAndEnqueueNotDispatchedReqs gets the items for which sending was not finished, cleans the storage
